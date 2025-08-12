57ae85ff500b3a2932176f1d0b9c5faa0e9c2a67
Abhinav Nautiyal <164336356+abhinavnautiyalDS@users.noreply.github.com>
2025-08-13 01:10:41 +0530

    Update App.py

--- App.py
+++ App.py
@@ -529,7 +529,7 @@ def main():
     <style>
     /* Set background for entire app including top and browser file areas */
     body, .stApp, .css-1aumxhk, .st-emotion-cache-1aumxhk {
-        background-image: url("https://images.pexels.com/photos/414144/pexels-photo-414144.jpeg?_gl=1*erczru*_ga*MTQ1MTM5MzE2OS4xNzU1MDI3Mjcx*_ga_8JE65Q40S6*czE3NTUwMjcyNzEkbzEkZzEkdDE3NTUwMjczMzAkajEkbDAkaDA.");
+        background-image: url("https://images.pexels.com/photos/3279307/pexels-photo-3279307.jpeg?_gl=1*69j723*_ga*MTQ1MTM5MzE2OS4xNzU1MDI3Mjcx*_ga_8JE65Q40S6*czE3NTUwMjcyNzEkbzEkZzEkdDE3NTUwMjc1OTkkajQ3JGwwJGgw");
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
