621412ab9832c957e8011896657029d919808494
Abhinav Nautiyal <164336356+abhinavnautiyalDS@users.noreply.github.com>
2025-08-13 12:08:43 +0530

    Update App.py

--- App.py
+++ App.py
@@ -533,7 +533,7 @@ def main():
     body, .stApp, .css-1aumxhk, .st-emotion-cache-1aumxhk {
         background-color: #FFFFFF; /* White background */
         background-image: url("https://plus.unsplash.com/premium_photo-1674502344608-87e3b5140254?q=80&w=1075&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"); 
-        color: #333333; /* Light black text for contrast */
+        color: #000000; /* Light black text for contrast */
     }
 
     /* Ensure sidebar matches light theme */
