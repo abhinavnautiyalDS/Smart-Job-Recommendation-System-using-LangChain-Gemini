793958bacdd6853de627ec595cfb02090812bb4f
Abhinav Nautiyal <164336356+abhinavnautiyalDS@users.noreply.github.com>
2025-08-13 12:09:25 +0530

    Update App.py

--- App.py
+++ App.py
@@ -532,7 +532,7 @@ def main():
     /* Set background for entire app including top and browser file areas */
     body, .stApp, .css-1aumxhk, .st-emotion-cache-1aumxhk {
         background-color: #FFFFFF; /* White background */
-        background-image: url("https://plus.unsplash.com/premium_photo-1674502344608-87e3b5140254?q=80&w=1075&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"); 
+        background-image: None
         color: #000000; /* Light black text for contrast */
     }
 
